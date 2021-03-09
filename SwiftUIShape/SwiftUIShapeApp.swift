21947ea377730639c9522cb9b044d3797aa7ceb0
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-09 16:27:20 +0800

    Initial Commit

---
+++ SwiftUIShape/SwiftUIShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapeApp.swift
+//  SwiftUIShape
+//
+//  Created by 廖昱晴 on 2021/3/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
